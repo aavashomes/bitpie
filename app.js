6240fbfb042f1af1eacd8eb3d5b680caea1f4cf8
mandiladitya <mandil.aditya@gmail.com>
2020-06-09 00:21:09 +0530

    MARK1

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-
-  var firebaseConfig = {
-    apiKey: "AIzaSyDJEaT4VliwgO3tMAfrwktXD6TQc9fN60w",
-    authDomain: "aavashomes.firebaseapp.com",
-    databaseURL: "https://aavashomes.firebaseio.com",
-    projectId: "aavashomes",
-    storageBucket: "aavashomes.appspot.com",
-    messagingSenderId: "416827212266",
-    appId: "1:416827212266:web:1148e3a34a848b5d9ab293",
-    measurementId: "G-Q03PT4Z9B4"
+var firebaseConfig = {
+    apiKey: "AIzaSyCOVRvLK4SdQBHw2BNAmPXF45Nhck8ddaY",
+    authDomain: "issac-8543d.firebaseapp.com",
+    databaseURL: "https://issac-8543d.firebaseio.com",
+    projectId: "issac-8543d",
+    storageBucket: "issac-8543d.appspot.com",
+    messagingSenderId: "314588981800",
+    appId: "1:314588981800:web:71aa8a1f04f88cd94e231d"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
